549f41bc703f104229cbfa8cd2da426cf1f34e1b
turnerj20 <turnerj20@vcu.edu>
2025-03-25 12:28:59 -0400

    Start code

---
+++ Sprint 1/AdherenceRecord.java
@@ -0,0 +1,14 @@
+import java.time.LocalDateTime;
+
+/**
+ * Represents a single instance of taking a medication on time
+ */
+public class AdherenceRecord {
+    private String medicationName;
+    private LocalDateTime timeTaken;
+
+    public AdherenceRecord(String medicationName, LocalDateTime timeTaken) {
+
+    }
+
+}
